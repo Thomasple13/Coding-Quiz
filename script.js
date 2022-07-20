09963710c934955764d78f12103fc40c9ac8141a
Thomas Le <tle1321@gmail.com>
2022-07-19 22:17:55 -0500

    some logic done

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 let timerEl = document.querySelector('.countdown')
 let startEl = document.querySelector('.start-button')
 let startScreenEl = document.querySelector('.start-screen')
+let quizScreenEl = document.querySelector('.quiz-screen')
+let resultScreenEl = document.querySelector('.result-screen')
+let questionEl = document.querySelector('.question')
+let choiceA = document.querySelector('#answer-button-a')
+let choiceB = document.querySelector('#answer-button-b')
+let choiceC = document.querySelector('#answer-button-c')
+let choiceD = document.querySelector('#answer-button-d')
+let choiceOutcome = document.querySelector(".choice-outcome")
 
-function countdown() {
-    let timeLeft = 60
-    var timeInterval = setInterval(function () {
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      timerEl.textContent = '';
-      clearInterval(timeInterval);
-    }
-  }, 1000);
+let score = 0
+let timeLeft = 60
+let choice = ''
+let currentQuestion = 0
+
+function reset(){
+  score = 0
+  timeLeft = 60
+  choice = ''
+  currentQuestion = 0
 }
 
-startEl.addEventListener("click",()=>{
-    countdown()
-    startScreenEl.style.display = 'none'
-})
-
-const questions = [
+const quiz = [
   {
     question: "How does a FOR loop start?",
     a: "for (i <= 5; i++)",
@@ -39,32 +37,85 @@ const questions = [
     b: "String",
     c: "Undefined",
     d: "NoneType",
-    correctAnswer: "D"
-  },
-  {
-    question: "",
-    answers: [
-    'a: ',
-    b: "",
-    c: "",
-    d: ""
-  ],
-    correctAnswer: 0
-  }
-  {
-    question: "",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-    correctAnswer: ""
-  }
-  {
-    question: "",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-    correctAnswer: ""
+    correctAnswer: "d"
   }
 ]
+  //Function that starts timer countdown
+function countdown() {
+    var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timeLeft--;
+      timerEl.textContent = timeLeft + ' seconds remaining';
+    } else if (timeLeft === 1) {
+      timeLeft--;
+      timerEl.textContent = timeLeft + ' second remaining';
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      quizDone();
+    }
+  }, 1000);
+}
+
+startEl.addEventListener("click",()=>{
+    reset()
+    timerEl.textContent = timeLeft + ' seconds remaining';
+    countdown()
+    startScreenEl.style.display = 'none'
+    quizScreenEl.style.display = 'block'
+    displayQuiz()
+})
+
+function displayQuiz (){
+    questionEl.textContent = quiz[currentQuestion].question
+    choiceA.textContent = quiz[currentQuestion].a
+    choiceB.textContent = quiz[currentQuestion].b
+    choiceC.textContent = quiz[currentQuestion].c
+    choiceD.textContent = quiz[currentQuestion].d
+}
+
+function checkAnswer(choice){
+  if(quiz[currentQuestion].correctAnswer === choice){
+    choiceOutcome.textContent = "Correct!"
+    score++
+    console.log('this is working')
+  } else{
+    choiceOutcome.textContent = "Incorrect!"
+    timeLeft -= 5
+    timerEl.textContent = timeLeft + ' seconds remaining';
+    score --
+  }
+  currentQuestion ++
+  if(currentQuestion < quiz.length){
+    displayQuiz()
+  }else{
+    quizDone()
+  }
+}
+
+function quizDone() {
+  console.log('quiz done')
+  quizScreenEl.style.display = 'none'
+  resultS
+}
+
+
+choiceA.addEventListener("click", ()=>{
+  choice = 'a'
+  checkAnswer(choice)
+})
+
+choiceB.addEventListener("click", ()=>{
+  choice = 'b'
+  checkAnswer(choice)
+
+})
+choiceC.addEventListener("click", ()=>{
+  choice = 'c'
+  checkAnswer(choice)
+})
+choiceD.addEventListener("click", ()=>{
+  choice = 'd'
+  checkAnswer(choice)
+})
